a60748e7c0ecb411a4921bd334fb73a528096fdf
Rachel Katz <rachel.katz@flatironschool.com>
2023-08-29 22:07:45 -0400

    solution done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title 
+        self._page_count = page_count
+
+    # @property 
+    # def page_count(self):
+    #     return self._page_count 
     
+    # @page_count.getter
+    # def page_count(self):
+    #     return self._page_count 
+    
+    # @page_count.setter
+    # def page_count(self, val):
+    #     if isinstance(val, int):
+    #         self._page_count = val 
+    #     else: 
+    #         print("page_count must be an integer")
+
+    def set_page_count(self, val):
+            if isinstance(val, int):
+                self._page_count = val 
+            else: 
+                print("page_count must be an integer")
+    def get_page_count(self):
+         return self._page_count
+    page_count = property(get_page_count, set_page_count)
+    
+    def turn_page(self):
+         print( "Flipping the page...wow, you read fast!")
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, condition="Old"):
+        self.brand = brand 
+        self._size = size 
+        self.condition = condition
+
+    @property
+    def size(self):
+        return self._size 
+    
+    @size.getter 
+    def size(self):
+        return self._size 
+    
+    @size.setter
+    def size(self, val):
+        if(isinstance(val, int)):
+            self._size = val 
+        else: 
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
