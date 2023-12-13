c665ed8aa75de70db460d267d32968e78cdd39b4
Rachel Katz <rachel.katz@flatironschool.com>
2023-12-13 14:05:19 -0500

    added decorator solution for properties

--- lib/shoe.py
+++ lib/shoe.py
@@ -6,20 +6,29 @@ class Shoe:
         self._size = size 
         self.condition = condition
 
-    @property
-    def size(self):
-        return self._size 
+    # @property
+    # def size(self):
+    #     return self._size 
     
-    @size.getter 
-    def size(self):
-        return self._size 
+    # @size.getter 
+    # def size(self):
+    #     return self._size 
     
-    @size.setter
-    def size(self, val):
+    # @size.setter
+    # def size(self, val):
+    #     if(isinstance(val, int)):
+    #         self._size = val 
+    #     else: 
+    #         print("size must be an integer")
+
+    def get_size(self):
+        return self._size 
+    def set_size(self, val):
         if(isinstance(val, int)):
             self._size = val 
         else: 
             print("size must be an integer")
+    size = property(get_size, set_size)
 
     def cobble(self):
         print("Your shoe is as good as new!")
